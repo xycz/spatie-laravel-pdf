e2f2055ba3c6debae5601382b34e26f7df8f5b3f
xycz <esandgr@gmail.com>
2024-01-14 19:07:18 +0100

    added show and index for pdf generation

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -2,20 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use Carbon\Carbon;
 use App\Models\Invoice;
+use Carbon\Carbon;
+use Spatie\LaravelPdf\Enums\Unit;
 use function Spatie\LaravelPdf\Support\pdf;
 
 class PdfController extends Controller
 {
-    public function __invoke(Invoice $invoice)
-    {        
+    public function index()
+    {
+        $invoices = Invoice::limit(1000)->get();
 
-        $now = Carbon::now()->format('Y-m-d H:i:s');
+        foreach ($invoices as $invoice) {
+            $invoice->items = json_decode($invoice->items, true);
+        }
 
         return pdf()
-        ->setNodeBinary('/usr/bin/node')
+            ->margins(32, 32, 32, 32, Unit::Pixel)
+            ->format('a4')
+            ->view('pdf.invoices', compact('invoices'))
+            ->name('invoices-'. now()->format('Y-m-d H:i:s') .'.pdf');
+    }
+
+    public function show(Invoice $invoice)
+    {            
+        $invoice->items = json_decode($invoice->items, true);
+
+        return pdf()
+            ->margins(32, 32, 32, 32, Unit::Pixel)
+            ->format('a4')
             ->view('pdf.invoice', compact('invoice'))
-            ->name('invoice-2023-04-10.pdf');
+            ->name('invoice-'. now()->format('Y-m-d H:i:s') .'.pdf');
     }
 }
